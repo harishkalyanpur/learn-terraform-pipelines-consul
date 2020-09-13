a1591014a7b62c54747e8cce67f5a97de59df279
harishkalyanpur <45255320+harishkalyanpur@users.noreply.github.com>
2020-09-13 12:02:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "hk-hashicorp-learn"
 
     workspaces {
-      name = "learn-terraform-pipelines-consul"
+      name = "hk-learn-terraform-pipelines-consul"
     }
   }
 }
